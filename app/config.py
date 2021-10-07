14c0ec6a53760ab86b8d07ae486fd0fb4d47b031
alexgQQ <alex.grand@beatport.com>
2021-10-06 21:05:33 -0600

    Refactor config to use an ini file

--- app/config.py
+++ app/config.py
@@ -1,4 +1,18 @@
+import os
+from collections import defaultdict
+from configparser import ConfigParser
 
-supported_formats = (
-    'jpeg', 'jpg', 'png',
-)
+# TODO: Should make a standardized location for this at some point
+config_file = 'wallflower.ini'
+config = ConfigParser()
+
+
+if os.path.exists(config_file):
+    config.read(config_file)
+else:
+    config['Core'] = defaultdict(None)
+    config['Reddit'] = defaultdict(None)
+    config['Imgur'] = defaultdict(None)
+    config['Wallhaven'] = defaultdict(None)
+    with open(config_file, 'w') as configfile:
+        config.write(configfile)
